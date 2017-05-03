116b1566e7dcbc62f2fa782e274ee3a8f7bf9b78
landrymercier <landry.mercier.37@gmail.com>
2017-05-03 08:56:15 +0200

    commit du 6/04/2017 - ajout textview (beta)

--- app/src/main/java/fr/plopez/mydailyway/MainActivity.java
+++ app/src/main/java/fr/plopez/mydailyway/MainActivity.java
@@ -2,6 +2,8 @@ package fr.plopez.mydailyway;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    TextView temperature = (TextView)findViewById(R.id.temperature);
+    TextView etat = (TextView)findViewById(R.id.etat);
+    TextView vitesseVent = (TextView)findViewById(R.id.vitesseVent);
+
+    public void afficheTemperature(View view) {
+        temperature.setText("Temperature");
+    }
+
+    public void afficheEtat(View view) {
+        etat.setText("Etat");
+    }
+
+    public void afficheVitesse(View view) {
+        vitesseVent.setText("Vitesse");
     }
 }
